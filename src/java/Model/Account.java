4c1aa3baa12d261b025295f21711bb466505574c
HUNGLVHE171796 <hunglvhe171796@fpt.edu.vn>
2024-07-04 01:11:08 +0700

    first commit

---
+++ src/java/Model/Account.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author toden
+ */
+public class Account {
+    int accountId;
+    String password;
+    String accountName;
+    String fullName;
+    String address;
+    boolean gender;
+    String email;
+    Role role;
+
+    public Account(int accountId, String password, String accountName, String fullName, String address, boolean gender, String email, Role role) {
+        this.accountId = accountId;
+        this.password = password;
+        this.accountName = accountName;
+        this.fullName = fullName;
+        this.address = address;
+        this.gender = gender;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Account() {
+        
+    }
+
+    public Account(int aInt, String string, String string0, String string1, String string2, boolean aBoolean, String string3, Role roleId, String string4) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+    
+}
